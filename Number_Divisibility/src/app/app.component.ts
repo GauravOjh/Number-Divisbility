9f8c816d1628c28235788618f09d0ab1d4feb4d3
Gaurav <gauravojha1111@gmail.com>
2024-06-15 16:50:10 +0530

    Directives

---
+++ Number_Divisibility/src/app/app.component.ts
@@ -0,0 +1,80 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,CommonModule,FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  isClearDisabled = true;
+  chosenNum = 1;
+  nums = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+    32,
+    33,
+    34,
+    35,
+    36,
+    37,
+    38,
+    39,
+    40,
+    41,
+    42,
+    43,
+    44,
+    45,
+    46,
+    47,
+    48,
+    49,
+    50,
+  ];
+
+  calculate(divisibleBy: number) {
+    this.chosenNum = divisibleBy;
+    if (divisibleBy === 1) {
+      this.isClearDisabled = true;
+    } else {
+      this.isClearDisabled = false;
+    }
+  }
+
+
+
+}
